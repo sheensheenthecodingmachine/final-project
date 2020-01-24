71cc7222dd5d5a513acab79fcabc01217467d505
Sheena-Antonios <antonios.sheena@gmail.com>
2020-01-24 19:10:47 +0100

    almost have a working POST request

---
+++ my-app/app.js
@@ -0,0 +1,25 @@
+//client code represents all the code that the browser executes
+//client code represents all the code that the user will see
+
+const clientPostFirstApiData = async (url = '/', firstApiData = {})=>{
+    console.log(firstApiData)
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+
+        },
+        body: JSON.stringify(firstApiData),
+    });
+try {
+    const newData = await response.json();
+    console.log(newData);
+    return newData
+}catch(error) {
+    console.log("error", error);
+    //handle the error
+}
+}
+
+clientPostFirstApiData('/', {weather: "subjective}"})
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 // a server receives a request, processes those requests and then returns a response
 // it is one the server that you create the routes
-
-
-
-
+// servers can be setup to save data produced while someone is within the app
 //express to run server and routes
 const express = require('express');
 // use express to start up an instance of your app
@@ -32,19 +29,21 @@ function listening() {
 }
 
 //JS object for data endpoint for first API route
-firstApiData = {};
+const firstApiData = {};
 
-//A GET route (requests data) uses url sheen requesting 
+//A GET route (requests data) uses root url requesting 
 //to send the data from the js-object "firstApiData"
 
 app.get('/', function getSomething (res, res){
     response.send(firstApiData);
 });
 
-//a POST route using url sheen and app instance,
-// Posts requests send and store data
+//a POST route using root url and app instance,
+// Post requests send and store data
+//i think they receieve data and store it and also send a specific response?
 
 app.post('/', function storeSomething(req, res){
-    console.log(req.body)
-    data.push(req.body)
+    FirstApiData.push(req.body)
+    console.log(firstApiData)
+    console.log("console should have logged identical things")
 })
