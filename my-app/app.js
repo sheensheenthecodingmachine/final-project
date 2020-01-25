b02ec8410090648e067cbfdf80d4db13b9f9bd84
Sheena-Antonios <antonios.sheena@gmail.com>
2020-01-25 19:29:17 +0100

    I think the get route to the api is correct and should return data related to the city if i manage to figure out what the correct url is

--- my-app/app.js
+++ my-app/app.js
@@ -2,18 +2,34 @@
 //client code represents all the code that the user will see
 
 
+
 //geonames API - function to send city name to API
 const url = 'http://api.geonames.org/citiesJSON?'
-const key = 'sheen';
+const key = 'sheen'
 
 //event listen + retrival of browser input
 document.getElementById("button-trip").addEventListener('click', getCityName);
 function getCityName(x){
     const cityName = document.getElementById("trip-input").value;
+    //getCityCoordinates (url, cityName, key);
     console.log(cityName);
-}
-//send the value of trip-input to the GeoName Api in a Get Request
 
+}
+
+// here you can update the ui so that trip-input.value displays in the app
+
+//send the value of trip-input to the GeoName Api in a Get Request
+// TODO-Async GET
+
+const getCityCoordinates = async (url= cityName, key)=>{
+    const repsonse = await fetch(longfunkyapiurl)
+    try{
+        const returnedData = await response.json();
+        console.log(returnedData)
+    } catch(error){
+        console.log("error", error);
+    }
+}
 
 /* Function to POST data along the Post route? */
 const appSendServerData = async (url = '/', firstApiData = {})=>{
