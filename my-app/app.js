e44ca7513afc82946e4cc77721d3ebab5828aadc
Sheena-Antonios <antonios.sheena@gmail.com>
2020-01-26 21:42:54 +0100

    this branch is good

--- my-app/app.js
+++ my-app/app.js
@@ -4,15 +4,14 @@
 //geonames API - function to send city name to API
 const url = "http://api.geonames.org/searchJSON?name="
 const key = "sheen"
+//const cityName = document.getElementById("trip-input").value
 
-//event listeners
-
-const globalCityName = document.getElementById("button-trip").addEventListener('click', function getCityName() {
-const cityName = document.getElementById("trip-input").value;
-document.getElementById('destination-output').innerHTML= cityName;
-})
+//event listen + retrival of browser input
+document.getElementById("button-trip").addEventListener('click', getCityName);
+function getCityName(x){
+    const cityName = document.getElementById("trip-input").value;
 //send the value of trip-input to the GeoName Api
-// Async GET/fetch to GERONAMES api
+// Async GET/fetch to api
 const getCityCoordinates = async (url, cityName, key)=>{
     const response = await fetch(`${url}${cityName}&maxRows=2&style=LONG&Lang=es&username=${key}`)
     try {
@@ -23,7 +22,9 @@ const getCityCoordinates = async (url, cityName, key)=>{
         console.log("error", error);
     }
 }
-/* Function to convert data from JSON and send as an object along the POST route to the storage endpoint in the server */
+/* Function to POST data along to save to our APP */
+// turn this into a then() function
+
 getCityCoordinates (url, cityName, key)
     .then(function(firstApiData){
         console.log(JSON.stringify(firstApiData));
@@ -31,25 +32,27 @@ getCityCoordinates (url, cityName, key)
         appSendServerData('/', {cityName: cityName, long: firstApiData.geonames[0].lng, lat: firstApiData.geonames[0].lat, country: firstApiData.geonames[0].countryName}
     )
     })
-
-    //then update the ui to display the destination
     .then(updateUI
-  )
-    updateUI = async() => {
-    const request = await fetch('/data')
-        console.log(request);
+
+    )
+}
+
+    const updateUI = async () => {
+    
+        const request = await fetch('/data')
+        console.log(request)
         try {
             const allData = await request.json()
-        console.log(allData)
-        const cityName = document.getElementById("trip-input").value;
-        
+            console.log(allData)
+            const cityName = document.getElementById("trip-input").value;
+        document.getElementById('destination-output').textContent = cityName;
         } catch(error){     
         console.log("error", error)
     }
 }
 /* Function to POST data */
 const appSendServerData = async ( url = '/', firstApiData = {})=>{
-        const response = await fetch(url, {
+      const response = await fetch(url, {
       method: 'POST', //GET POST PUT DELETE
       credentials: 'same-origin',
       headers: {
@@ -65,23 +68,13 @@ const appSendServerData = async ( url = '/', firstApiData = {})=>{
         return newData;
 
       } catch(error) {
-      console.log("error", error)
+      console.log("error", error);
 }
 }
 
-//event listeners
+// here you can update the ui so that trip-input.value displays in the app
 
-dateClickEventListener();
-function dateClickEventListener() {
-  document.getElementById("button-date").addEventListener('click', functionToGetDate);
-}
 
-  function functionToGetDate(x){
-  const getDate = document.getElementById("date-input").value;
-  document.getElementById("departure-date-output").textContent = getDate;
-  console.log(getDate)
- 
-}
 //testing event listeners
 var test = document.getElementById("button-trip");
 test.onclick = function sayHi() {
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+  
 // a server receives a request, processes those requests and then returns a response via ROUTES
 
 const express = require('express');
@@ -26,12 +27,10 @@ function listening() {
 }
 
 //A GET route (requests data)
-app.get('/data', function getFirstApiData(res, req){
-//console.log(firstApiData)
+app.get('/data', function getSomething (res, res){
+console.log(firstApiData)
 res.send(firstApiData);
 }); 
-
-
 
 //JS object for data endpoint for first API route
 const firstApiData = [];
@@ -39,45 +38,23 @@ const firstApiData = [];
 //a POST route - store data recieved to app endpoint
 //add the send data to our app
 app.post('/', storeSomething);
-function storeSomething(req, res){
+async function storeSomething(req, res){
+  const futureWeather = await darkSkyApi(lat, lng)
   storage = { 
     
     cityName: req.body.cityName, 
     long: req.body.long, 
     lat: req.body.lat, 
-    country: req.body.country,
-    high: "",
-    low: "",
-    description: "",
-    date: ""
+    country: req.body.country
   }
 
     firstApiData.push(storage)
     console.log(firstApiData)
-
-//Dark Sky API INfo
-const darkSkyKey = "a0b392ae344130eba769f60cdac088b1"
-const darkSkyURL = "https://api.darksky.net/forecast/"
+}
 
 // build dark sky api async function
-const getFutureWeatherFromApi = async (darkSkyURL, long, lat, darkSkyKey, getDate)=>{
-  const response = await fetch(`${darkSkyURL}${darkSkyKey}/${lat},${long},${getDate}`)
-  try {
-      const WeatherData = await response.json()
-      console.log(WeatherData)
-      return WeatherData;
-  } catch(error){
-   
-    console.log("error", error);
-  }
-  }
 
-/* Function to convert data from JSON and send as an object along the POST route to the storage endpoint in the server */
-getFutureWeatherFromApi (darkSkyURL, long, lat, darkSkyKey, getDate)
-  .then(function(secondApiData){
-      console.log(JSON.stringify(secondApiData));
-      console.log(secondApiData , "recieved from SKY whatever API")
-      postToStorage('/', {date: getDate}
-  )
-  })
+async function darkSkyApi(lat, lng){
+
+  return futureWeather 
 }
